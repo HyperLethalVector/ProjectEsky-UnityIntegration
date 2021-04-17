9a55869c1099ac61e7e06f6a420c9f58512e650d
Dr.Damo <hyperlethalvector92@gmail.com>
2021-04-17 12:59:07 +0930

    Got something ready to test on the network for creating an offer

--- Assets/Scripts/Esky/Networking/UDPAutoDiscovery.cs
+++ Assets/Scripts/Esky/Networking/UDPAutoDiscovery.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
 using System;
@@ -7,30 +8,88 @@ using System.Text;
 using System.Linq;
 using System.ComponentModel;
 using System.Threading;
+using Microsoft.MixedReality.WebRTC;
+using System.Threading.Tasks;
 
 namespace ProjectEsky.Networking.Discovery{
-    public class UDPAutoDiscovery : MonoBehaviour
+    public class PackageManagerHookBehaviour : Microsoft.MixedReality.WebRTC.Unity.Signaler{
+        public delegate void BytesReceivedDelegate(byte[] b);
+        public BytesReceivedDelegate BytesReceived;
+        public virtual void SendBytes(byte[] b){
+            
+        }
+        public override Task SendMessageAsync(SdpMessage message)
+        {
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public override Task SendMessageAsync(IceCandidate candidate)
+        {
+            return null;
+        }
+    }
+    
+
+    [System.Serializable]
+    public class IceMessageWebsocket{
+        public string candidate;
+        public int sdpMlineIndex;
+        public string sdpMid;
+        public IceMessageWebsocket(string cand, int sdpmline, string sdpmid){
+            candidate = cand;
+            sdpMlineIndex = sdpmline;
+            sdpMid = sdpmid;
+        }
+    }
+    [System.Serializable]
+    public class SdpMessageWebsocket{
+        public string sdp;
+        public string type;
+        public SdpMessageWebsocket(string sdpMessage, string sdpType){
+            sdp = sdpMessage;
+            type = sdpType; 
+        }
+    }
+    
+    [System.Serializable]
+    public class WebrtcShakeString{
+        public List<string> IceMessages;
+        public string sdpMessage;
+    }
+    [System.Serializable]
+    public class WebrtcShakeClass{
+        public List<IceMessageWebsocket> iceMessages;
+        public SdpMessageWebsocket sdpMessage;
+    }
+    public delegate void CreateOfferDelegate();
+    public delegate void CreateResponseDelegate();
+
+    public class UDPAutoDiscovery : PackageManagerHookBehaviour
     {
         BackgroundWorker objWorkerDiscovery;
         AutoDiscoverySender ads;
         AutoDiscoveryReceiver adr;
+        public bool hasMessagePrepared = false;
+        public WebrtcShakeString shake = new WebrtcShakeString();
         public void Start() {
             objWorkerDiscovery = new BackgroundWorker();
             objWorkerDiscovery.WorkerReportsProgress = true;
             objWorkerDiscovery.WorkerSupportsCancellation = true;
-            adr = new AutoDiscoveryReceiver(ref objWorkerDiscovery);
+            adr = new AutoDiscoveryReceiver(ref objWorkerDiscovery,CreateOffer,this);
             objWorkerDiscovery.DoWork += new DoWorkEventHandler(adr.Start);
             objWorkerDiscovery.ProgressChanged += new ProgressChangedEventHandler(LogProgressChanged);
             objWorkerDiscovery.RunWorkerAsync();
         }
         public void StartSender(){
+            shake.IceMessages.Clear();
             objWorkerDiscovery.CancelAsync();
             adr.Stop();
             adr = null;
             objWorkerDiscovery = new BackgroundWorker();
             objWorkerDiscovery.WorkerReportsProgress = true;
             objWorkerDiscovery.WorkerSupportsCancellation = true;
-            ads = new AutoDiscoverySender(ref objWorkerDiscovery);
+            ads = new AutoDiscoverySender(ref objWorkerDiscovery,CreateAnswer,this);
             objWorkerDiscovery.DoWork += new DoWorkEventHandler(ads.Start);
             objWorkerDiscovery.ProgressChanged += new ProgressChangedEventHandler(LogProgressChanged);
             objWorkerDiscovery.RunWorkerAsync();            
@@ -50,10 +109,66 @@ namespace ProjectEsky.Networking.Discovery{
                 adr.Stop();
             }
         }
+        
+        public void CreateOffer(){
+            PeerConnection.StartConnection();
+        }
+        public void CreateAnswer(){
+            PeerConnection.Peer.CreateAnswer();
+        }
+
+        public void ReceiveCompletedOffer(WebrtcShakeClass receivedClass){
+            Debug.Log("Received Offer: " + receivedClass.sdpMessage.sdp);
+            SdpMessage sdpOffer = new SdpMessage { Type = SdpMessageType.Offer, Content = receivedClass.sdpMessage.sdp};
+            PeerConnection.HandleConnectionMessageAsync(sdpOffer).ContinueWith(_ =>
+            {
+                // If the remote description was successfully applied then immediately send
+                // back an answer to the remote peer to acccept the offer.
+                _nativePeer.CreateAnswer();
+            }, TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.RunContinuationsAsynchronously);     
+        }
+        public void ReceiveCompletedAnswer(WebrtcShakeClass receivedClass){
+            Debug.Log("Received Answer: " + receivedClass.sdpMessage.sdp);
+            SdpMessage sdpAnswer = new SdpMessage { Type = SdpMessageType.Answer, Content = receivedClass.sdpMessage.sdp};                            
+            PeerConnection.HandleConnectionMessageAsync(sdpAnswer).ContinueWith(_ =>
+            {
+                Debug.Log("Handled Answer");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.RunContinuationsAsynchronously);
+        }
+        public void ReceiveIceCandidate(WebrtcShakeClass receivedClass){
+            for(int i = 0; i < receivedClass.iceMessages.Count; i++){
+                _nativePeer.AddIceCandidate(new IceCandidate{SdpMid = receivedClass.iceMessages[i].sdpMid,SdpMlineIndex = receivedClass.iceMessages[i].sdpMlineIndex,Content = receivedClass.iceMessages[i].candidate});
+            }
+        }
+        
+        protected override void OnIceCandidateReadyToSend(IceCandidate candidate)
+        {
+          //Data = string.Join(IceSeparatorChar, candidate.Content, candidate.SdpMlineIndex.ToString(), candidate.SdpMid);      
+            IceMessageWebsocket imws = new IceMessageWebsocket(candidate.Content,candidate.SdpMlineIndex,candidate.SdpMid);
+            string json = JsonUtility.ToJson(imws);  
+            shake.IceMessages.Add(json);
+        }
+        protected override void OnSdpOfferReadyToSend(SdpMessage offer)
+        {
+
+            SdpMessageWebsocket sdpws = new SdpMessageWebsocket(offer.Content,"offer");
+            Debug.Log("Sending Offer: " + sdpws.sdp);                        
+            string json = JsonUtility.ToJson(sdpws);
+            shake.sdpMessage = json;
+        }
+        protected override void OnSdpAnswerReadyToSend(SdpMessage answer)
+        {
+
+            SdpMessageWebsocket sdpws = new SdpMessageWebsocket(answer.Content,"answer");
+            Debug.Log("Sending Answer: " + sdpws.sdp);
+            string json = JsonUtility.ToJson(sdpws);
+            shake.sdpMessage = json;            
+        }
     }
     public class AutoDiscoveryReceiver
     {
-
+        public CreateOfferDelegate createOfferDelegate;
+        public UDPAutoDiscovery hookedAutoDiscovery;
         private System.ComponentModel.BackgroundWorker workerUDP;
 
         // Port the UDP server will listen to broadcast packets from UDP Clients.
@@ -72,11 +187,13 @@ namespace ProjectEsky.Networking.Discovery{
 
         private bool disposing = false;
 
-        public AutoDiscoveryReceiver(ref BackgroundWorker workerUDP)
+        public AutoDiscoveryReceiver(ref BackgroundWorker workerUDP, CreateOfferDelegate offerDelegate,UDPAutoDiscovery hookedDiscovery)
         {
             this.workerUDP = workerUDP;
             this.BroadCastDaemonPort = AutoDiscoveryPort;
             this.addrDaemonListenIP = IPAddress.Parse("0.0.0.0");
+            this.createOfferDelegate = offerDelegate;
+            this.hookedAutoDiscovery = hookedDiscovery;
         }
 
         public void Stop()
@@ -128,11 +245,12 @@ namespace ProjectEsky.Networking.Discovery{
 
                         // Here we reply the Service IP and Port (TCP).. 
                         // You must point to your server and service port. For example a webserver: sending the correct IP and port 80.
-
-                        byte[] packetBytesAck = Encoding.ASCII.GetBytes("ACK " + GetLocalIPAddress() + " " + BroadCastDaemonPort); // Acknowledged
-
+                        createOfferDelegate.Invoke();
+                        Thread.Sleep(5000);//wait for the candidates to be generated
+                        string s = JsonUtility.ToJson(hookedAutoDiscovery.shake);
+                        this.workerUDP.ReportProgress(1, "Got discovered, sending offer: " + s);
+                        byte[] packetBytesAck = Encoding.ASCII.GetBytes("ACK=" + s); // Acknowledged
                         newsock.Send(packetBytesAck, packetBytesAck.Length, RemoteEP);
-
                         this.workerUDP.ReportProgress(1, "Answering(ACK) " + packetBytes.Length + " bytes to " + IncomingIP);
                     }
                     else
@@ -158,7 +276,8 @@ namespace ProjectEsky.Networking.Discovery{
     }
     public class AutoDiscoverySender
         {
-
+            public CreateResponseDelegate createAnswerDelegate;
+            public UDPAutoDiscovery hookedAutoDiscovery;
             public bool disposing = false;
 
             // Fixed Port for broadcast.
@@ -183,10 +302,12 @@ namespace ProjectEsky.Networking.Discovery{
 
             private BackgroundWorker worker;
 
-            public AutoDiscoverySender(ref BackgroundWorker worker)
+            public AutoDiscoverySender(ref BackgroundWorker worker, CreateResponseDelegate offerDelegate, UDPAutoDiscovery hookedDiscovery)
             {
                 this.worker = worker;
                 worker.ReportProgress(1, "AutoDiscoverySender::Started at " + AutoDiscoveryPort + "/UDP");
+                this.hookedAutoDiscovery = hookedDiscovery;
+                this.createAnswerDelegate = offerDelegate;
             }
 
             public void Start(object sender, DoWorkEventArgs e)
